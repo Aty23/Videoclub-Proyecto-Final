b7bdfb6b41764cd003b8d0759ef298c092a63f51
llorenc <@>
2023-05-30 18:53:50 +0200

    Avance FormAlqilar,

--- proyecto_final_videoclub/src/view/FormAlquilar.java
+++ proyecto_final_videoclub/src/view/FormAlquilar.java
@@ -6,73 +6,54 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FormAlquilar extends JFrame {
+    String[] listaTest ={"1","2","3","4","5","6","1","2","3","4","5","6"};
+    String[] listaTest2 ={"a","b","c","d","e","f","a","b","c","d"};
+    String[] listaTest3 ={"!","@","¢","!","@","¢",};
     Color colorFondo=new Color(147,249,249);
-    Color colorLetraBoton=new Color(37,52,124);
-    Color colorheisen=new Color(37,52,124);
-    Color colorButtons= new Color(255,255,255);
-    JComboBox cmbEligeMultimedia = new JComboBox<>();
-
-    JList lstDiscos = new JList<>();
-    JList lstVideojuegos= new JList<>();
 
     public JPanel panelPrincipal = new JPanel();
     public FormAlquilar() throws HeadlessException{
-        JLabel a=new JLabel("a");
-        a.setBounds(500,500,200,200);
-        panelPrincipal.add(a);
-        a.setVisible(false);
-        JLabel b=new JLabel("b");
-        b.setBounds(500,500,200,200);
-        panelPrincipal.add(b);
-        b.setVisible(false);
-        JLabel c=new JLabel("c");
-        c.setBounds(500,500,200,200);
-        panelPrincipal.add(c);
-        c.setVisible(false);
 
+        JComboBox cmbEligeMultimedia = new JComboBox<>();
         cmbEligeMultimedia.setBounds(100,100,350,50);
         panelPrincipal.add(cmbEligeMultimedia);
         panelPrincipal.setLayout(null);
         panelPrincipal.setBackground(colorFondo);
 
-        JList lstPeliculas = new JList<>();
-        lstPeliculas.setBounds(100,200,350,50);
+        JTextField txtfldMulti = new JTextField(20);
+        txtfldMulti.setBounds(100,200,350,30);
+        txtfldMulti.setVisible(true);
+        panelPrincipal.add(txtfldMulti);
+
+        JButton btnBuscar = new JButton("Buscar");
+        btnBuscar.setBounds(500,200,100,30);
+        btnBuscar.setVisible(true);
+        panelPrincipal.add(btnBuscar);
+
+        JButton btnAlquilar = new JButton("Alquilar");
+        btnAlquilar.setBounds(500,500,100,30);
+        btnAlquilar.setVisible(true);
+        panelPrincipal.add(btnAlquilar);
+
+        JLabel lblAlquiler = new JLabel("El precio es:");
+        lblAlquiler.setBounds(100,500,100,30);
+        lblAlquiler.setVisible(true);
+        panelPrincipal.add(lblAlquiler);
+
+        JList lstPeliculas = new JList<>(listaTest);
         panelPrincipal.add(lstPeliculas);
-        lstPeliculas.setVisible(false);
-        lstPeliculas.setVisibleRowCount(10);
+        lstPeliculas.setVisibleRowCount(listaTest.length);
         lstPeliculas.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         JScrollPane scrollerPeliculas = new JScrollPane(lstPeliculas);
-        scrollerPeliculas.setBounds(100,200,350,50);
+        scrollerPeliculas.setBounds(100,300,350,100);
         scrollerPeliculas.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scrollerPeliculas.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollerPeliculas.setVisible(false);
         panelPrincipal.add(scrollerPeliculas);
+        scrollerPeliculas.setVisible(true);
 
-        JList lstDiscos = new JList<>();
-        lstDiscos.setBounds(100,200,350,50);
-        panelPrincipal.add(lstDiscos);
-        lstDiscos.setVisible(false);
-        lstDiscos.setVisibleRowCount(10);
-        lstDiscos.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        JScrollPane scrollerDiscos = new JScrollPane(lstPeliculas);
-        scrollerDiscos.setBounds(100,200,350,50);
-        scrollerDiscos.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollerDiscos.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollerDiscos.setVisible(false);
-        panelPrincipal.add(scrollerDiscos);
 
-        JList lstVideojuegos= new JList<>();
-        lstVideojuegos.setBounds(100,200,350,50);
-        panelPrincipal.add(lstVideojuegos);
-        lstVideojuegos.setVisible(false);
-        lstVideojuegos.setVisibleRowCount(10);
-        lstVideojuegos.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        JScrollPane scrollerVideojuegos = new JScrollPane(lstPeliculas);
-        scrollerVideojuegos.setBounds(100,200,350,50);
-        scrollerVideojuegos.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollerVideojuegos.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollerVideojuegos.setVisible(false);
-        panelPrincipal.add(scrollerVideojuegos);
+
+
 
 
         try{
@@ -90,42 +71,11 @@ public class FormAlquilar extends JFrame {
         public void actionPerformed(ActionEvent e) {
             if(cmbEligeMultimedia.getSelectedItem().toString().equals("Películas")){
 
-                a.setVisible(true);
-                b.setVisible(false);
-                c.setVisible(false);
-
-                lstPeliculas.setVisible(true);
-                scrollerPeliculas.setVisible(true);
-                lstDiscos.setVisible(false);
-                scrollerDiscos.setVisible(false);
-                lstVideojuegos.setVisible(false);
-                scrollerVideojuegos.setVisible(false);
 
             }else if(cmbEligeMultimedia.getSelectedItem().toString().equals("Discos")){
 
-                a.setVisible(false);
-                b.setVisible(true);
-                c.setVisible(false);
-
-                lstDiscos.setVisible(true);
-                scrollerDiscos.setVisible(true);
-                lstPeliculas.setVisible(false);
-                scrollerPeliculas.setVisible(false);
-                lstVideojuegos.setVisible(false);
-                scrollerVideojuegos.setVisible(false);
-
             }else if(cmbEligeMultimedia.getSelectedItem().toString().equals("Videojuegos")){
 
-                a.setVisible(false);
-                b.setVisible(false);
-                c.setVisible(true);
-
-                lstVideojuegos.setVisible(true);
-                scrollerVideojuegos.setVisible(true);
-                lstPeliculas.setVisible(false);
-                scrollerPeliculas.setVisible(false);
-                lstDiscos.setVisible(false);
-                scrollerDiscos.setVisible(false);
 
             }
         }
